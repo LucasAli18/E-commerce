7ce43889513170dc4053a41914db265473544f6d
Lucas Ali <lucas.10.ali@gmail.com>
2022-06-22 09:49:21 -0300

    Simulador Interactivo

--- js/main.js
+++ js/main.js
@@ -19,21 +19,21 @@ function mostrarProductos(){
       total += sweater;
       carrito += "Sweater \n";
       alert("Se agrego: sweater al carrito");
-      alert(carrito)
+      alert("Su carrito es:\n" +carrito + "\nTotal de $"+total)
       mostrarProductos();
     break
     case "2":
       total += jogger;
       carrito += "jogger \n";
       alert("Se agrego: jogger al carrito");
-      alert(carrito)
+      alert("Su carrito es:\n" +carrito + "\nTotal de $"+total)
       mostrarProductos();
     break
     case "3":
       total += remera;
       carrito += "remera \n";
       alert("Se agrego: remera al carrito");
-      alert(carrito)
+      alert("Su carrito es:\n" +carrito + "\nTotal de $"+total)
       mostrarProductos();
       break
     case "4":
@@ -44,12 +44,12 @@ function mostrarProductos(){
       mostrarProductos();
       break
     case "5":
-      alert("Su carrito es:\n" + carrito +"\n" + "$"+ total);
+      alert("Su carrito es:\n" + carrito + "\nTotal de $"+total);
       mostrarProductos();
       break
     case "6":
       alert("Gracias por su compra");
-      alert("Su carrito es:\n" + carrito +"\n" + "$"+ total);
+      alert("Su carrito es:\n" + carrito + "\nTotal de $"+total);
       carrito = "";
       total = 0;
       break
@@ -65,149 +65,33 @@ function mostrarProductos(){
 
 function decidir(){
   let decision = prompt("Estas con ganas de renovar tu outfit "+ nombre +"? \n Si \n No ");
-  while(decision === ""){
+  while(decision === "") {
     decision = prompt("Estas con ganas de renovar tu outfit "+ nombre +"? \n Si \n No ");
   }
     return decision
 }
 
-
+//Declaro las variables
 let total = 0;
 let carrito = "";
 let sweater = 4000;
 let jogger = 4700;
 let campera = 5000;
 let remera = 2000;
+
+//Llamo la funcion de solicitarNombre
 let nombre = solicitarNombre()
+
+//Llamo la funcion de decidir
 let decision = decidir()
     if((decision==="Si") || (decision==="si")|| (decision==="sI") || (decision==="SI")){
-    mostrarProductos()
+
+      //Llamo la funcion de mostrar productos
+
+       mostrarProductos()
+
     }
+
     else if((decision==="No") || (decision==="no")|| (decision==="nO") || (decision==="NO")){
       alert("Gracias vuelva pronto")
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function AñadirCarrito(){
-  if ((producto==="1")&&(opcion==="1")){
-    alert("Añadiste al carrito sweater escote V " + "con un costo de "+ sweater);
-    Total = Total + sweater;
-  }
-
-}
-
-function CompraProd(){
-let producto = prompt("Que estas pensando comprar? \n 1: Sweater \n 2: Jogger \n 3: Campera")
-
-      switch (producto){
-        case "1":
-
-          alert("Nada mal! Nuestros sweaters son exelente calidad")
-          opcion=prompt("Contamos de dos tipos de sweater escote V y cuello redondo, selecciona uno: \n 1:Escote V \n 2:Cuello redondo")
-
-          if (opcion==="1"){
-            alert("Añadiste al carrito sweater escote V, una buena opcion para combinarlo con tus camisas")
-            carrito += "sweaterV"
-            Total += sweaterV
-            alert("Tu carrito es: "+ carrito + Total);
-
-          }
-
-          else if(opcion==="2"){
-            alert("Seleccionaste el sweater escote redondo, una buena opcion para combinarlo con remeras");
-            alert("Añadiste al carrito sweater cuello redondo, una buena opcion para combinarlo con remera");
-            carrito += "sweaterC";
-            Total += sweaterC;
-            alert("Tu carrito es: "+ carrito);
-          }
-
-          break;
-
-          case "2":
-
-            alert("Excelente decision, nuestros joggers son de lo mejor");
-
-            break;
-
-          case "3":
-
-            alert("Increible, elegiste nuestras camperas, no te vas a arrepentir, contamos con una amplia variedad de colores")
-
-            break;
-          }
-      } */
-
-  
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*       //Variables
-      let campera = 5500
-      let jogger = 4700
-      let sweaterV = 4000
-      let sweaterC = 4000
-      let carrito = ""
-      let Total = 0
-
-      SolicitarNombr()
-
-      let decision = prompt("Estas con ganas de renovar tu outfit "+ nombre +"? \n Si \n No ");
-
-if((decision==="Si") || (decision==="si")|| (decision==="sI") || (decision==="SI")){
-  CompraProd()
-    }
-    else if((decision==="No") || (decision==="no")|| (decision==="nO") || (decision==="NO")){
-      alert("Gracias vuelva pronto")
-    }
-
-    decision = prompt("Excelente decision! como seguimos? \n1:Desea seguir comprando \n2:Ver carrito \n 3:Finalizar")
-    switch (decision){
-      case "1":
-      CompraProd();
-      break;
-
-      case "2":
-        alert("Carrito: \n{carrito} \n${Total}")
-        break;
-
-      case "3":
-        alert("Gracias por la compra su total es \n${Total}")
-        Total=0;
-        carrito="";
-        break;
-    } */
