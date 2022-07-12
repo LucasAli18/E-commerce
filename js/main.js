40049fdba79712be47a47ac31035ecebe262488f
Lucas Ali <lucas.10.ali@gmail.com>
2022-07-11 22:17:25 -0300

    Agregando-Evento

--- js/main.js
+++ js/main.js
@@ -2,34 +2,34 @@
 let stock = [
   {
       id: 1,
-      nombre: "Jogger Azul",
-      precio: 5100,
+      nombre: "Campera",
       imagen:"./img/SW_lanilla_A.jpg",
   },
   {
-      id: 1,
-      nombre: "Jogger Azul",
-      precio: 5100,
+      id: 2,
+      nombre: "Sweatter",
       imagen:"./img/SW_lanilla_B.jpg",
   },
   {
-      id: 1,
-      nombre: "Jogger Azul",
-      precio: 5100,
+      id: 3,
+      nombre: "Jogger",
       imagen:"./img/SW_lanilla_G.jpg",
   },
   {
-      id: 1,
-      nombre: "Jogger Azul",
-      precio: 5100,
+      id: 4,
+      nombre: "Bermuda",
       imagen:"./img/SW_lanilla_N.jpg",
   },
   {
-      id: 1,
-      nombre: "Jogger Azul",
-      precio: 5100,
+      id: 5,
+      nombre: "Basica",
       imagen:"./img/SW_lanilla_V.jpg",
-  }
+  },
+  {
+    id: 6,
+    nombre: "Botones",
+    imagen:"./img/SW_lanilla_V.jpg",
+}
   
 ]
 
@@ -43,6 +43,7 @@ let pantalon = document.getElementById("pantalon");
 let precio2 = parseInt(pantalon.options[pantalon.selectedIndex].value);
 let remera = document.getElementById("remera");
 let precio3 = parseInt(remera.options[remera.selectedIndex].value);
+boton.innerHTML= `<button type="button" id="button" class="text-success" value="Calcular total">Gracias</button>`
 let suma = `Total a pagar es $ ${precio1 + precio2 + precio3}`;
 document.getElementById("resultado").value = suma;
 };
